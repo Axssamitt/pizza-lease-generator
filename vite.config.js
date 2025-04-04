64c2626028dd62b32595e24d82ff1c390624b5c5
Axssamitt <jsrmturk@gmail.com>
2025-04-04 20:29:35 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,28 @@
+
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/jphcontract/",
-});
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "0.0.0.0", // Allow external access
+    port: 8080,
+  },
+  preview: {
+    host: "0.0.0.0", // Allow external access for preview mode as well
+    port: 4173,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/jphcontract?",
+}));
